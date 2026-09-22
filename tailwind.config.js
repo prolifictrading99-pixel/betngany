59a4679f48c4951849c2cb1696b6da1ae81eb261
betngany <prolifictrading99@gmail.com>
2026-09-22 22:15:37 +0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: { colors: { brand: "#16a34a" } } },
   plugins: [],
-};
+}
